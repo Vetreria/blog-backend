b30d322077d42daef6314d7cdf1fe3bf60ae6f11
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-04-02 01:49:00 +0300

    Оживили

---
+++ verstka2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\root\documents\python\verstka2\blog-backend\verstka2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
